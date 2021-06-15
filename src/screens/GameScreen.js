c2efec76a267e868ed4d691ea64912a986dc052b
Chris Leech <chris.leech@gmail.com>
2021-06-15 16:30:39 -0700

    confirm before losing game data

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,13 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StatusBar} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import RosterList from '../components/RosterList';
 
 import {useStore} from '../stores/RootStore';
 
-// import NavRail from '../components/NavRail';
-import {Appbar, Text} from 'react-native-paper';
-import {CommonActions} from '@react-navigation/native';
+import {Snackbar} from 'react-native-paper';
 
 import {
   useDeviceOrientation,
@@ -30,33 +28,6 @@ function LandscapeView(props) {
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, flexDirection: 'row'}}>
-        {/* <NavRail>
-          <Appbar.BackAction
-            onPress={() => props.navigation.dispatch(CommonActions.goBack())}
-          />
-          <Text />
-          <Appbar.Action
-            icon="play-circle-outline"
-            onPress={() => {
-              props.navigation.navigate('Game Play');
-            }}
-          />
-          <Text style={{textAlign: 'center'}}>Game Manager</Text>
-          <Appbar.Action
-            icon="cards-outline"
-            onPress={() => {
-              props.navigation.navigate('Library');
-            }}
-          />
-          <Text style={{textAlign: 'center'}}>Card Library</Text>
-          <Appbar.Action
-            icon="cogs"
-            onPress={() => {
-              props.navigation.navigate('Settings');
-            }}
-          />
-          <Text style={{textAlign: 'center'}}>Settings</Text>
-        </NavRail> */}
         <RosterList teams={[store.team1]} />
         <RosterList teams={[store.team2]} />
       </View>
@@ -69,9 +40,31 @@ export default function GameScreen(props) {
   const {height, width} = useDimensions().window;
   const landscape = width > height;
 
-  if (landscape) {
-    return <LandscapeView {...props} />;
-  } else {
-    return <PortraitView {...props} />;
-  }
+  const [showSnack, setShowSnack] = useState(false);
+  const [navEvent, saveNavEvent] = useState(null);
+
+  useEffect(() =>
+    props.navigation.addListener('beforeRemove', e => {
+      e.preventDefault();
+      saveNavEvent(e);
+      setShowSnack(true);
+    }),
+  );
+
+  return (
+    <View style={{flex: 1, width: '100%'}}>
+      {landscape ? <LandscapeView {...props} /> : <PortraitView {...props} />}
+      <Snackbar
+        visible={showSnack}
+        onDismiss={() => setShowSnack(false)}
+        action={{
+          label: 'Exit',
+          onPress: () => {
+            props.navigation.dispatch(navEvent.data.action);
+          },
+        }}>
+        Leaving This Screen Will Exit The Game
+      </Snackbar>
+    </View>
+  );
 }
