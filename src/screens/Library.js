a44caa9188b26f58ba8e22eb28b22c7c07c47822
Chris Leech <chris.leech@gmail.com>
2021-06-15 18:27:28 -0700

    use guild grid for card library
    clean up dead code from library screen

--- src/screens/Library.js
+++ src/screens/Library.js
@@ -3,14 +3,14 @@ import {ScrollView, View} from 'react-native';
 import {withTheme, Chip, Text} from 'react-native-paper';
 import {useDimensions} from '@react-native-community/hooks';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import LinearGradient from 'react-native-linear-gradient';
-import MaskedView from '@react-native-community/masked-view';
 
 import {displayName} from '../components/GuildData';
-import GBIcon from '../components/GBIcons';
 
 import {useData} from '../components/DataContext';
 import CardCarousel from '../components/CardCarousel';
+
+import GuildGrid, {itemSize} from '../components/GuildGrid';
+import ImageBackground from 'react-native/Libraries/Image/ImageBackground';
 
 const TeamLibrary = withTheme(props => {
   const gdata = useData();
@@ -94,118 +94,38 @@ const LibraryScreen = withTheme(props => {
   const Guilds = data.Guilds;
 
   return (
-    <SafeAreaView
-      edges={
-        landscape
-          ? ['top', 'bottom', 'left', 'right']
-          : ['bottom', 'left', 'right']
-      }
-      style={{
-        flex: 1,
-        flexDirection: 'row',
-      }}>
-      {/* background */}
-      <View
+    <ImageBackground
+      source={require('../assets/library.jpg')}
+      style={{width: '100%', height: '100%', alignItems: 'center'}}
+      imageStyle={{resizeMode: 'cover'}}>
+      <SafeAreaView
+        edges={
+          landscape
+            ? ['top', 'bottom', 'left', 'right']
+            : ['bottom', 'left', 'right']
+        }
         style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-        }}>
-        <MaskedView
-          style={{flex: 1}}
-          maskElement={
-            <View
-              style={{
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: 'transparent',
-                // backgroundColor: 'black',
-              }}>
-              <GBIcon name="GB" size={380} color="#fff" />
-            </View>
-          }>
-          <View
-            style={{
-              flex: 1,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <LinearGradient
-              colors={['#eee', '#888', '#222']}
-              style={{
-                width: '100%',
-                height: 400,
-              }}
-            />
-          </View>
-        </MaskedView>
-      </View>
-      {/* {landscape && (
-        <NavRail>
-        <Appbar.Action />
-          <Text />
-          <Appbar.Action
-            icon="play-circle-outline"
-            onPress={() => {
-              props.navigation.navigate('Game Play');
-            }}
-            />
-          <Text style={{textAlign: 'center'}}>Game Manager</Text>
-          <Appbar.Action
-            icon="cards-outline"
-            onPress={() => {
-              props.navigation.navigate('Library');
-            }}
-            />
-          <Text style={{textAlign: 'center'}}>Card Library</Text>
-          <Appbar.Action
-            icon="cogs"
-            onPress={() => {
-              props.navigation.navigate('Settings');
-            }}
-            />
-          <Text style={{textAlign: 'center'}}>Settings</Text>
-          </NavRail>
-      )} */}
-      <View
-        style={{
+          flex: 1,
           flexDirection: 'row',
-          width: '100%',
-          height: '100%',
         }}>
         <View
           style={{
-            flex: -1,
-            flexWrap: 'wrap',
+            flexDirection: 'row',
+            width: '100%',
             height: '100%',
-            backgroundColor: props.theme.colors.surface,
           }}>
-          {Guilds.map(g => (
-            <Chip
-              key={g.name}
-              onPress={() => {
-                props.navigation.navigate(g.name);
-              }}>
-              {g.name}
-            </Chip>
-            // <Drawer.Item
-            //   style={{width: 110}}
-            //   label={g.name}
-            //   key={g.name}
-            //   onPress={() => {
-            //     props.navigation.navigate(g.name);
-            //   }}
-            // />
-          ))}
+          <GuildGrid
+            pickTeam={guild => {
+              props.navigation.navigate(guild);
+            }}
+          />
+
+          <Text style={{position: 'absolute', bottom: 0, right: 0}}>
+            [{version}]
+          </Text>
         </View>
-        <Text style={{position: 'absolute', bottom: 0, right: 0}}>
-          [{version}]
-        </Text>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </ImageBackground>
   );
 });
 
