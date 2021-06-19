a202734a4979b1e99a73c34ada499941b3568a0a
Chris Leech <chris.leech@gmail.com>
2021-06-18 21:07:05 -0700

    rework settings

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,96 +1,117 @@
 import React, {useState} from 'react';
-import {View} from 'react-native';
-import {withTheme, Title, Text, RadioButton, Divider} from 'react-native-paper';
+import {View, ScrollView} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {
+  withTheme,
+  Title,
+  Text,
+  TextInput,
+  RadioButton,
+  Divider,
+} from 'react-native-paper';
 import DeviceInfo from 'react-native-device-info';
 
-import {Picker} from '@react-native-picker/picker';
+import DropDown from 'react-native-paper-dropdown';
 import {useStore} from '../stores/RootStore';
 import {Observer} from 'mobx-react-lite';
 
 import {useData} from '../components/DataContext';
 
 const SettingsView = withTheme(props => {
+  const theme = props.theme;
   const store = useStore();
   const settings = store.settings;
   const {manifest} = useData();
 
+  const [showScreenDropDown, setScreenDropDown] = useState(false);
+  const [showDataDropDown, setDataDropDown] = useState(false);
+
   return (
-    <Observer>
-      {() => (
-        <View>
-          <Title>{`GB Playbook ${DeviceInfo.getReadableVersion()}`}</Title>
+    <SafeAreaView edges={['bottom', 'left', 'right']}>
+      <Observer>
+        {() => (
+          <ScrollView>
+            <Title>{`GB Playbook ${DeviceInfo.getReadableVersion()}`}</Title>
 
-          <Divider />
-
-          <View>
-            <Title>Season and Errata Version:</Title>
-            {store.draftReady ? (
-              <Text>Dataset cannot be changed while a game is active.</Text>
-            ) : <></>}
-            <Picker
-              mode="dropdown"
-              enabled={!store.draftReady}
-              selectedValue={settings.dataSet}
-              onValueChange={(itemValue, itemIndex) => {
-                settings.setDataSet(itemValue);
-              }}>
-              {manifest.datafiles.map(dataSet => (
-                <Picker.Item
-                  key={dataSet.version}
-                  style={{
-                    color: props.theme.colors.text,
-                    backgroundColor: props.theme.colors.background,
+            <Divider style={{marginVertical: 15}} />
+            <View>
+              <Title>Season and Errata Version:</Title>
+              {store.draftReady ? (
+                <Text>Season cannot be changed while a game is active.</Text>
+              ) : (
+                <></>
+              )}
+              <View style={{marginHorizontal: 10}}>
+                <DropDown
+                  value={settings.dataSet}
+                  setValue={settings.setDataSet}
+                  list={manifest.datafiles.map(dataSet => ({
+                    label: `[${dataSet.version}] ${dataSet.description}`,
+                    value: dataSet.filename,
+                  }))}
+                  mode="outlined"
+                  visible={showDataDropDown}
+                  showDropDown={() => {
+                    if (!store.draftReady) {
+                      setDataDropDown(true);
+                    }
                   }}
-                  label={`[${dataSet.version}] ${dataSet.description}`}
-                  value={dataSet.filename}
+                  onDismiss={() => setDataDropDown(false)}
+                  inputProps={{
+                    right: <TextInput.Icon name="menu-down" />,
+                    editable: false,
+                  }}
                 />
-              ))}
-            </Picker>
-          </View>
+              </View>
+            </View>
 
-          <Divider />
+            <Divider style={{marginVertical: 15}} />
+            <View>
+              <Title>Inital Screen:</Title>
+              <View style={{marginHorizontal: 10}}>
+                <DropDown
+                  value={settings.initialScreen}
+                  setValue={settings.setInitialScreen}
+                  list={[
+                    {label: 'Game Play', value: 'Game Play'},
+                    {label: 'Card Library', value: 'Library'},
+                  ]}
+                  mode="outlined"
+                  visible={showScreenDropDown}
+                  showDropDown={() => setScreenDropDown(true)}
+                  onDismiss={() => setScreenDropDown(false)}
+                  inputProps={{
+                    right: (
+                      <TextInput.Icon name="menu-down" numberOfLines={1} />
+                    ),
+                    editable: false,
+                  }}
+                />
+              </View>
+            </View>
 
-          <View>
-            <Title>App Theme:</Title>
-            <RadioButton.Group
-              value={settings.colorScheme}
-              onValueChange={newValue => settings.setColorScheme(newValue)}>
-              <RadioButton.Item label="Light Theme" value="light" />
-              <RadioButton.Item label="Dark Theme" value="dark" />
-            </RadioButton.Group>
-          </View>
-
-          <Divider />
-
-          <View>
-            <Title>Inital Screen:</Title>
-            <Picker
-              mode="dropdown"
-              selectedValue={settings.initialScreen}
-              onValueChange={(itemValue, itemIndex) => {
-                settings.setInitialScreen(itemValue);
-              }}>
-              <Picker.Item
-                style={{
-                  color: props.theme.colors.text,
-                  backgroundColor: props.theme.colors.background,
-                }}
-                label="Game Play"
-                value="Game Play"
-              />
-              <Picker.Item
-                style={{
-                  color: props.theme.colors.text,
-                  backgroundColor: props.theme.colors.background,
-                }}
-                label="Card Library"
-                value="Library"
-              />
-            </Picker>
-          </View>
-        </View>
-      )}
-    </Observer>
+            <Divider style={{marginVertical: 15}} />
+            <View>
+              <Title>App Theme:</Title>
+              <RadioButton.Group
+                value={settings.colorScheme}
+                onValueChange={newValue => settings.setColorScheme(newValue)}>
+                <RadioButton.Item
+                  color={theme.colors.primary}
+                  label="Light "
+                  value="light"
+                />
+                <RadioButton.Item
+                  color={theme.colors.primary}
+                  label="Dark"
+                  value="dark"
+                />
+              </RadioButton.Group>
+            </View>
+          </ScrollView>
+        )}
+      </Observer>
+    </SafeAreaView>
   );
 });
 
