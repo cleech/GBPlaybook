4c6eb944604db877723e6e1c95cc16c64e08caab
Chris Leech <cleech@redhat.com>
2023-02-05 17:17:47 -0800

    use metered.ca TURN servers

--- src/services/webrtc.tsx
+++ src/services/webrtc.tsx
@@ -6,14 +6,28 @@ import React, {
   ReactNode,
 } from "react";
 
-export const iceConfig = {
+const iceConfig = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
     { urls: "stun:global.stun.twilio.com:3478" },
     { urls: "stun:relay.metered.ca:80" },
+    {
+      urls: "turn:relay.metered.ca:80",
+      username: process.env.REACT_APP_METERED_USERNAME,
+      credential: process.env.REACT_APP_METERED_PASSWORD,
+    },
+    {
+      urls: "turn:relay.metered.ca:443",
+      username: process.env.REACT_APP_METERED_USERNAME,
+      credential: process.env.REACT_APP_METERED_PASSWORD,
+    },
+    {
+      urls: "turn:relay.metered.ca:443?transport=tcp",
+      username: process.env.REACT_APP_METERED_USERNAME,
+      credential: process.env.REACT_APP_METERED_PASSWORD,
+    },
   ],
 };
-
 interface RTCContextValues {
   pc?: RTCPeerConnection;
   dc?: RTCDataChannel;
