140ab91d7471d21cbc730e68475e8f342f98d46b
Chris Leech <cleech@redhat.com>
2023-07-24 10:44:24 -0700

    update deps, fix vite polyfill for rooks?

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig, Plugin } from 'vite';
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import {nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +10,12 @@ export default defineConfig({
     react(),
     viteTsconfigPaths(),
     svgrPlugin(),
+    nodePolyfills({
+	    globals: {
+		    global: true,
+	    },
+	    protocolImports: false,
+    }),
   ],
   build: {
     outDir: 'build',
