3134671f9fc8a7e809bb9718232b6e444e03eaca
Chris Leech <cleech@redhat.com>
2023-09-05 11:37:10 -0700

    finish vite migration, use PWA plugin

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+// import reportWebVitals from "./reportWebVitals";
 
 import {
   createHashRouter,
@@ -72,15 +72,15 @@ rootStorePersist().then(() => {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({
-  onUpdate: (registration) => {
-    // console.log(
-    //   `SW update callback, have waiting sw? ${registration.waiting != null}`
-    // );
-  },
-});
+//serviceWorkerRegistration.register({
+//  onUpdate: (registration) => {
+//    // console.log(
+//    //   `SW update callback, have waiting sw? ${registration.waiting != null}`
+//    // );
+//  },
+//});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,62 @@
-import { defineConfig, Plugin } from 'vite';
-import react from '@vitejs/plugin-react';
-import viteTsconfigPaths from 'vite-tsconfig-paths';
-import svgrPlugin from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteTsconfigPaths(),
-    svgrPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        // cache all imports
+        globPatterns: ['**/*']
+      },
+      // cache all public folder static assets
+      includeAssets: ['**/*'],
+      manifest: {
+        name: 'Guild Ball Playbook',
+        short_name: 'GB Playbook',
+        description: 'Guild Ball reference and tracking',
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "48x48 32x32 16x16",
+            type: "image/x-icon"
+          },
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any"
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any"
+          },
+          {
+            src: "gbpb-logo-full.png",
+            type: "image/png",
+            sizes: "1024x1024",
+            purpose: "maskable"
+          }
+        ],
+        display: "standalone",
+        orientation: "any",
+        theme_color: "#000000",
+        background_color: "#004508"
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
   ],
   define: {
-	  global: "globalThis",
-  },
-  build: {
-    outDir: 'build',
+    global: 'globalThis'
   },
   server: {
-    port: 3000,
-  },
-});
+    port: 3000
+  }
+})
