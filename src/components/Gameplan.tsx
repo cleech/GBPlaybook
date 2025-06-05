4b590594e81c4f4b9257becf63b0a0ed626f539c
Chris Leech <chris.leech@gmail.com>
2025-06-04 21:50:18 -0700

    fix Gameplan white-space for card export

--- src/components/Gameplan.tsx
+++ src/components/Gameplan.tsx
@@ -54,7 +54,7 @@ export const GameplanFront = (props: {
           }}
         >
           <div
-            style={{
+            className={css({
               fontFamily: "IM Fell Great Primer SC",
               fontSize: "33.33pt",
               display: "flex",
@@ -62,7 +62,9 @@ export const GameplanFront = (props: {
               alignItems: "center",
               lineHeight: "0.8em",
               margin: "0.2em 0",
-            }}
+              whiteSpace: "pre",
+              '& span': { whiteSpace: "pre" }
+            })}
           >
             { /* ignore the dropcap style if it's all caps */
               /[a-z]/.test(gameplan.title) ?
@@ -82,16 +84,16 @@ export const GameplanFront = (props: {
           <div
             // 10pt scaled at 200dpi/96 is 20.83pt
             // 9pt (Keep Your Chin Up) is 18.75pt
-            className={css`
-              margin: 0 1em;
-              white-space: pre-wrap;
-              display: flex;
-              flex-direction: column;
-              align-items: center;
-              line-height: 1;
-              p { font-size: 20.83pt; margin: 0em 0 1em 0; }
-              #KeepYourChinUp { font-size: 18.75pt; }
-            `}
+            className={css({
+              margin: "0 1em",
+              whiteSpace: "pre",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              lineHeight: 1,
+              p: { fontSize: '20.83pt', margin: '0em 0 1em 0', whiteSpace: 'pre' },
+              'p#KeepYourChinUp': { fontSize: '18.75pt' }
+            })}
           >
             <p id={gameplan.title.replace(/[^a-zA-Z0-9]+/g, '')}>
               {gameplan.text}
