dd2c68bbfa890ddfecfaf2979ba43a8b0f4b69d0
Chris Leech <chris.leech@gmail.com>
2025-06-11 14:05:53 -0700

    redo icons by hand
    I don't like what pwa-asset-generator was doing with padding.
    I guess it assumed a minimal icon, when I want to provide a maskable one
    and crop when it makes sense.
    
    following advice from:
    https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs

--- vite.config.ts
+++ vite.config.ts
@@ -83,11 +83,6 @@ export default defineConfig({
         description: 'Guild Ball reference and tracking',
         icons: [
           {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
             src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
@@ -96,7 +91,6 @@ export default defineConfig({
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any',
           },
           {
             src: 'maskable-icon-512x512.png',
