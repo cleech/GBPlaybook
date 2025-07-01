088d4bc80776d44eb17fd3199c1d4a6aca827f1c
Chris Leech <chris.leech@gmail.com>
2025-07-01 09:47:41 -0700

    fix build ts issue with rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import { visualizer } from 'rollup-plugin-visualizer';
@@ -111,7 +111,7 @@ export default defineConfig({
         enabled: true,
       },
     }),
-    visualizer(),
+    visualizer() as PluginOption,
   ],
   define: {
     BUILD_DATE: JSON.stringify(new Date().toDateString()),
