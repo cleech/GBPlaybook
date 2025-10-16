a8137bd0704c34eb4b104656fb4ddd4484e8c1b7
Chris Leech <chris.leech@gmail.com>
2025-10-16 16:51:43 -0700

    Translations: ZH: Preview

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import '@fontsource/crimson-text';
 import '@fontsource/im-fell-great-primer-sc';
 import '@fontsource/im-fell-great-primer';
 import '@fontsource/noto-sans-symbols';
+import '@fontsource/noto-serif-sc';
 
 import { lazy } from "react";
 import { createRoot } from "react-dom/client";
